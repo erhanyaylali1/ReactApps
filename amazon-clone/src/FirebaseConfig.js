596de75387c98e7c17784f486e32d8b7f436063f
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2021-02-28 19:37:57 +0300

    Amazon Clone initialize

---
+++ amazon-clone/src/FirebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBk7PO7ruaEQ8NRcmHoOslhbmkexGXJiQk",
+    authDomain: "ey1-f69b8.firebaseapp.com",
+    projectId: "ey1-f69b8",
+    storageBucket: "ey1-f69b8.appspot.com",
+    messagingSenderId: "332302051403",
+    appId: "1:332302051403:web:5be0cebac174a7fdc2c5ce",
+    measurementId: "G-HKHLZKP41P"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
---
+++ amazon-clone/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from '../features/counterSlice';
+
+export default configureStore({
+  reducer: {
+    app: appReducer,
+  },
+});
