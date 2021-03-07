4850177152a60d7d340b9fbea9fb57994601b75e
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2021-03-07 19:34:02 +0300

    v1.1 card shadow

--- Shopping App/src/Components/Products/Product/styles.js
+++ Shopping App/src/Components/Products/Product/styles.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
     root: {
         maxWidth: '100%',
+        '&:hover': {
+            boxShadow: '0px 0px 4px 1px rgba(0,0,0,0.75)'
+        }
     },
     media: {
         height: 0,
