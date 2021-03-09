7da4555261eeac30646ec843bd31bea9a2e64a38
Erhan Yaylalı <32177766+erhanyaylali1@users.noreply.github.com>
2021-03-09 22:33:57 +0300

    like comment done

--- Social Media/functions/db_schema.js
+++ Social Media/functions/db_schema.js
@@ -1,12 +1,47 @@
 export const db = {
+
+    users: [
+        {
+            email: '',
+            username: '',
+            name: '',
+            surname: '',
+            phone: '',
+            password: '',
+            createdAt: '',
+            imageUrl: '',
+            bio : '',
+            location : '',
+            website : ''
+        },
+    ],
+
     posts: [
         {
             userId: '',
             content: '',
-            createdAt: '2020-03-08 19.40.01.0000',
-            likeCount: 5,
-            commentCount: 2
+            createdAt: '',
+            likesCount: 0,
+            commentsCount: 0
+        }
+    ],
+
+    comments: [
+        {
+            userId: '',
+            postId: '',
+            content: '',
+            createdAt: ''
+        }
+    ],
+
+    likes: [
+        {
+            userId: '',
+            postId: '',
+            createdAt: ''
         }
     ]
+
 }
 
--- Social Media/functions/index.js
+++ Social Media/functions/index.js
@@ -19,57 +19,6 @@ firebase.initializeApp(firebaseConfig);
 const db = admin.firestore();
 
 
-app.get('/posts', (req, res) => {
-
-    db.collection('posts')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then((data) => {
-        let posts = [];
-        data.forEach((doc) => {
-            posts.push({
-                postId: doc.id,
-                content: doc.data().content,
-                userId: doc.data().userId,
-                createdAt: doc.data().createdAt,
-                likeCount: doc.data().likeCount,
-                commentCount: doc.data().commentCount
-            });
-        });
-        return res.json(posts);
-    })
-    .catch((err) => console.error(err));
-});
-
-
-app.post('/createpost', (req, res) => {
-
-    if(req.method !== "POST"){
-        return res.status(400).json({ error: "Method not allowed." });
-    } else {
-
-        const newPost = {
-            content: req.body.content,
-            userId: req.body.userId,
-            createdAt: new Date().toISOString(),
-            likeCount: 0,
-            commentCount: 0
-        };
-    
-        db.collection('posts')
-            .add(newPost)
-            .then(() => {
-                return res.json({
-                    message: "Post Succesfully Created."
-                });
-            })
-            .catch((err) => {
-                return res.json(err);
-            })
-    }
-});
-
-
 app.post('/signup', (req, res) => {    
 
     const newUser = {
@@ -79,7 +28,8 @@ app.post('/signup', (req, res) => {
         name: req.body.name,
         surname: req.body.surname,
         phone: req.body.phone,
-        createdAt: new Date().toLocaleDateString()
+        createdAt: new Date().toLocaleDateString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/socialony.appspot.com/o/no-img.png?alt=media&token=d7d666ee-60dd-4487-ba4b-ef76e961fe4b`
     };
 
     db.collection('users')
@@ -107,7 +57,6 @@ app.post('/signup', (req, res) => {
         });
 });
 
-
 app.post('/login', (req, res) => {
 
     firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
@@ -128,42 +77,260 @@ app.post('/login', (req, res) => {
     )
 });
 
+app.post('/user/add-detail', (req, res) => {
+    
+    const userDetails = {
+        bio: req.body.bio,
+        website: req.body.website,
+        location: req.body.location
+    };
+    
+    db.collection('users').doc(req.body.userId).update(userDetails)
+    .then(() => {
+        return res.json({ message: "Details Added Successfully."});
+    })
+    .catch((err) => {
+        return res.status(500).json({ error: err.message })
+    })
+})
+
+app.get('/user', (req, res) => {
+
+    let userData = {};
+    db.collection('users')
+        .doc(req.body.userId)
+        .get()
+        .then((doc) => {
+            userData.credentials = doc.data();
+            return db.collection('posts')
+                    .where('userId', '==', req.body.userId)
+                    .orderBy('createdAt', 'desc')
+                    .get();
+        })
+        .then((data) => {
+            userData.posts = []
+            if(data){
+                data.forEach((doc) => {
+                    userData.posts.push(doc.data());
+                })
+            }
+            return res.json(userData);
+        })
+        .catch((err) => {
+            return res.status(500).json({ error: err.message })
+        })
+})
 
 app.post('/user/image', (req, res) => {
 
-    const BusBoy = require('busboy');
-    const path = require('path');
-    const os = require('os');
-    const fs = require('fs');
-
+    const BusBoy = require("busboy");
+    const path = require("path");
+    const os = require("os");
+    const fs = require("fs");
+    const { uuid } = require("uuidv4");
+  
     const busboy = new BusBoy({ headers: req.headers });
 
     let imageFileName;
     let imageToBeUploaded = {};
+    let generatedToken = uuid();
 
-    busboy.once('file', (fieldname, file, filename, encoding, mimetype) => {
-        const imageExtension = filename.split('.')[filename.split('.').length - 1];
-        imageFileName = `${Math.round(Math.random() * 100000000)}.${imageExtension}`;
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+        console.log(fieldname, file, filename, encoding, mimetype);
+        if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+          return res.status(400).json({ error: "Wrong file type submitted" });
+        }
+        const imageExtension = filename.split(".")[filename.split(".").length - 1];
+        imageFileName = `${Math.round(Math.random() * 1000000000000).toString()}.${imageExtension}`;
         const filepath = path.join(os.tmpdir(), imageFileName);
         imageToBeUploaded = { filepath, mimetype };
         file.pipe(fs.createWriteStream(filepath));
-    });
+      });
 
     busboy.on('finish', () => {
-        admin.storage().bucket().upload(imageToBeUploaded.filepath, {
+        admin.storage()
+        .bucket()
+        .upload(imageToBeUploaded.filepath, {
             resumable: false,
             metadata: {
                 metadata: {
-                    contentType: imageToBeUploaded.mimetype
-                }
-            }
+                    contentType: imageToBeUploaded.mimetype,
+                    firebaseStorageDownloadTokens: generatedToken,
+                },
+            },
+        })
+        .then(() => {
+            const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${firebaseConfig.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
+            db.collection('users').doc(req.body.id).update({ imageUrl });
+            return;
+        })
+        .then(() => {
+            return res.json({ message: "Image Uploaded Succesfully." });
+        })
+        .catch((err) => {
+            return res.status(500).json({ error: err.message });
         })
     })
-    .then(() => {
-        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${firebaseConfig.storageBucket}/o/${imageFileName}?alt=media`;
-        console.log(imageUrl);
-    })
-
+    
+    busboy.end(req.rawBody);  
 })
+
+app.get('/posts', (req, res) => {
+
+    db.collection('posts')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+        let posts = [];
+        data.forEach((doc) => {
+            posts.push({
+                postId: doc.id,
+                content: doc.data().content,
+                userId: doc.data().userId,
+                createdAt: doc.data().createdAt,
+                likeCount: doc.data().likeCount,
+                commentCount: doc.data().commentCount
+            });
+        });
+        return res.json(posts);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post('/createpost', (req, res) => {
+
+    if(req.method !== "POST"){
+        return res.status(400).json({ error: "Method not allowed." });
+    } else {
+
+        db.collection('users')
+            .doc(req.body.userId)
+            .get()
+            .then((doc) => doc.data().imageUrl)
+            .then((imageUrl) => {
+
+                const newPost = {
+                    content: req.body.content,
+                    userId: req.body.userId,
+                    createdAt: new Date().toISOString(),
+                    likesCount: 0,
+                    commentsCount: 0,
+                    imageUrl
+                };
+                db.collection('posts')
+                .add(newPost)
+                .then(() => res.json({ message: "Post Succesfully Created." }));
+            })
+            .catch((err) => {
+                return res.json(err);
+            })
+    }
+});
+
+app.get('/post/:postId', (req, res ) => {
+
+    let postData = {};
+    db.collection('posts')
+        .doc(req.params.postId)
+        .get()
+        .then((doc) => {
+            if(!doc.exists) return res.status(404).json({ error: "Post not found." });
+            postData = doc.data();
+            postData.id = doc.id;
+            return db.collection('comments')
+                .where('postId', '==', req.params.postId)
+                .orderBy('createdAt', 'asc')
+                .get();
+                
+        })
+        .then((data) => {
+            postData.comments = [];
+            if(data){
+                data.forEach((doc) => {
+                    postData.comments.push(doc.data());
+                });
+            }            
+            return res.json(postData);
+        })
+        .catch((err) => {
+            return res.status(500).json({ message: err.message});
+        })
+})
+
+app.post('/post/:postId/comment', (req, res) => {
+
+    let commentsCount = 0;
+    db.collection('posts')
+        .doc(req.params.postId)
+        .get()
+        .then((doc) => {
+            if(!doc) return res.json({ error: "Post Not Found!" });
+            commentsCount = doc.data().commentsCount + 1;
+            return commentsCount
+        })
+        .then((commentsCount) => {
+            db.collection('posts')
+                .doc(req.params.postId)
+                .update({ commentsCount })
+        })
+        .then(() => {
+            return db.collection('users')
+                .doc(req.body.userId)
+                .get()
+                .then((doc) => doc.data().imageUrl)
+        })
+        .then((imageUrl) => {
+            console.log(imageUrl);
+            const newComment = {
+                content: req.body.content,
+                createdAt: new Date().toISOString(),
+                postId: req.params.postId,
+                userId: req.body.userId,
+                imageUrl
+            };
+            db.collection('comments')
+                .add(newComment)
+                .then(() => res.json({ message: "Comment Posted Successfully! "}));
+        })
+        .catch((err) => res.status(500).json({ error: err.message }));      
+})
+
+app.post('/post/:postId/like', (req, res) => {
+    var query = db.collection('likes').where('userId','==', req.body.userId);
+    query = query.where('postId','==',req.params.postId);
+    query.get()
+        .then((doc) => {
+            if(doc._size) return res.status(404).json({ error: 'This Post Already Liked' }); 
+            else {
+                db.collection('posts')
+                    .doc(req.params.postId)
+                    .get()
+                    .then((doc) => doc.data().likesCount + 1)
+                    .then((likesCount) => {
+                        db.collection('posts')
+                            .doc(req.params.postId)
+                            .update({ likesCount })
+                    })
+                    .then(() => {
+                        const newLike = {
+                            userId: req.body.userId,
+                            postId: req.params.postId,
+                            createdAt: new Date().toISOString()
+                        };
+                        db.collection('likes')
+                            .add(newLike)
+                            .then(() => res.json({ message: "Post Liked Successfully! "}));
+                    })
+            }
+        })
+        .catch((err) => res.status(500).json({ error: err.message }));
+})
+
+
+// TO DO
+// UNLIKE A POST
+// DELETE A POST
+// DELETE A COMMENT
+// FOLLOW A USER
 
 exports.api = functions.https.onRequest(app);
